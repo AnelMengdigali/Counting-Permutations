d67250f9ca507412654f96e4eba27b05c4a4700b
AnelMengdigali <54207471+AnelMengdigali@users.noreply.github.com>
2020-08-03 23:24:45 +0600

    Update and rename Ex3.java to CountPerm.java

--- CountPerm.java
+++ CountPerm.java
@@ -51,19 +51,3 @@ class Result{
     }
     
 }
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-        
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
-        
-        int result = Result.countPerms(n);
-        
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-        
-        bufferedReader.close();
-        bufferedWriter.close();
-    }
-}
